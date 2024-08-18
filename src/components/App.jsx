39995f65fb7ef9d29dcce29ac83106bb893f9088
Finley <finley.mwachia12@gmail.com>
2024-08-19 02:56:34 +0300

    add Avatar Component to App Component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import Card from "./Card";
 import contacts from "../contacts";
-
+import Avatar from "./Avatar";
 function App() {
   return (
     <div>
       <h1 className="heading">My Contacts</h1>
-
-      <Card
+      <Avatar img ="https://miro.medium.com/v2/resize:fit:2400/1*8OkdLpw_7VokmSrzwXLnbg.jpeg"/>
+     <Card
         name={contacts[0].name}
         img={contacts[0].imgURL}
         tel={contacts[0].phone}
